42651a3f7a4e67c8574c5f6656b8d5f23f7b4546
Marc CB <marc.cbrooks@gmail.com>
2017-10-31 18:13:21 -0400

    add reactive stream to window size

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,75 @@
 <template>
   <div id="app">
-    
+    <h1>{{ win.type }}</h1>
+    <h4>Width: {{ win.size }}</h4>
   </div>
 </template>
 
 <script>
+import xs from "xstream";
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      win: {
+        size: null,
+        type: "FullHD"
+      },
+      windowListener: {
+        next: value => {
+          this.setSize(value)
+        },
+        error: err => {
+          console.log("Error from window stream: ", err);
+        },
+        complete: () => {
+          console.log("Window stream is complete.");
+        }
+      }
+    };
+  },
+  computed: {
+    producer() {
+      return {
+        start(listener) {
+          window.onresize = event => {
+            listener.next(event);
+          };
+        },
+        stop() {
+          console.log("No longer listening to window size.");
+        }
+      };
+    },
+    main$() {
+      return xs.createWithMemory(this.producer);
+    },
+    window$() {
+      return xs.from(this.main$).map(v => v.target.innerWidth);
+    }
+  },
+  mounted() {
+    this.setSize(window.innerWidth);
+    this.window$.addListener(this.windowListener);
+  },
+  methods: {
+    setSize(width) {
+      this.win.size = width;
+      if (this.win.size >= 1408) {
+        this.win.type = "FullHD";
+      } else if (this.win.size < 1408 && this.win.size >= 1216) {
+        this.win.type = "Widescreen";
+      } else if (this.win.size < 1216 && this.win.size >= 1024) {
+        this.win.type = "Desktop";
+      } else if (this.win.size < 1024 && this.win.size >= 769) {
+        this.win.type = "Tablet";
+      } else {
+        this.win.type = "Mobile";
+      }
     }
   }
-}
+};
 </script>
 
 <style>
